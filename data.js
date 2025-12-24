e69cd1affae3cf685d98ef9a85393a1f0fbd6d6a
ivanopvichs-cyber <ivanopvichs@gmail.com>
2025-12-23 21:41:35 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,211 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama_casa-moctezuma-8-ivan--_2024-12-19-16-38-012",
+      "name": "Panorama_CASA MOCTEZUMA 8 IVAN  _2024-12-19-16-38-012",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7409681916076174,
+          "pitch": 0.5887826494541777,
+          "rotation": 0,
+          "target": "5-panorama_casa-moctezuma-8-ivan--_2024-12-19-17-02-00"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-panorama_casa-moctezuma-8-ivan--_2024-12-19-16-40-37",
+      "name": "Panorama_CASA MOCTEZUMA 8 IVAN  _2024-12-19-16-40-37",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-panorama_casa-moctezuma-8-ivan--_2024-12-19-16-47-37",
+      "name": "Panorama_CASA MOCTEZUMA 8 IVAN  _2024-12-19-16-47-37",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-panorama_casa-moctezuma-8-ivan--_2024-12-19-16-50-58",
+      "name": "Panorama_CASA MOCTEZUMA 8 IVAN  _2024-12-19-16-50-58",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-panorama_casa-moctezuma-8-ivan--_2024-12-19-16-54-51",
+      "name": "Panorama_CASA MOCTEZUMA 8 IVAN  _2024-12-19-16-54-51",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-panorama_casa-moctezuma-8-ivan--_2024-12-19-17-02-00",
+      "name": "Panorama_CASA MOCTEZUMA 8 IVAN  _2024-12-19-17-02-00",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1557135832398835,
+          "pitch": 0.7781570616421654,
+          "rotation": 0,
+          "target": "0-panorama_casa-moctezuma-8-ivan--_2024-12-19-16-38-012"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
